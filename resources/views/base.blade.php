8997ced3d7dacbe2bb76bd766bdbf902ba106b0d
freecree <bodya20126@gmail.com>
2020-11-05 19:23:06 +0200

    change title in base

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Iz Vladeniya Akello @yield('title')</title>
+    <title>@yield('title') | Iz Vladeniya Akello </title>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
